4a4b5096464a279222ba468c2980c1b5b1efe447
michaelkantor <mkantor@vmware.com>
2012-09-13 16:27:44 -0700

    Reimplements the page

--- projects/BookShare/webapproot/pages/BookDetails/BookDetails.a.js
+++ projects/BookShare/webapproot/pages/BookDetails/BookDetails.a.js
@@ -6,25 +6,59 @@ _end: 0
 });
 
 BookDetails.widgets = {
+designTimeData: ["wm.Variable", {"json":"{\"author\":[{\"dataValue\":\"Elizabeth Wood\"},{\"dataValue\":\"Justine Howard\"},{\"dataValue\":\"Pat Broadhead\"}],\"availableToDownload\":0,\"bookshareId\":\"548376\",\"briefSynopsis\":\"Providing high quality play experiences is an essential part of good early years education, but this can pose a challenge for practitioners who face pressure from a more didactic primary curriculum, and from parents worried that their children will \",\"category\":[{\"dataValue\":\"Nonfiction\"}],\"completeSynopsis\":\"Providing high quality play experiences is an essential part of good early years education, but this can pose a challenge for practitioners who face pressure from a more didactic primary curriculum, and from parents worried that their children will fail to acquire essential skills and knowledge.  By helping the reader to develop their understanding of the complex relationships between play and learning, this book examines current theoretical perspectives on play, alongside examples of recent and innovative play research from a range of disciplinary and methodological perspectives.  With contributions from leading play scholars, it brings together theory, research, policy and practice in relation to play and learning in early years settings.  The emphasis is on the relationship between play and learning, and play and pedagogy, and the need to understand these dimensions more substantially in order to teach with confidence.  Included are chapters on: - the influence of play on thinking, problem-solving and creativity - cooperative play and learning - play, risk and outdoor learning - learning to play in cultural context There are chapter objectives, reflective points, reflective tasks and suggestions for further reading throughout, to facilitate critical thinking and encourage independent study.  Suitable for early years practitioners, early childhood students at undergraduate and postgraduate levels, and all those who work with and care for young children, this is an exciting and thought-provoking book.\",\"contentId\":548376,\"copyright\":\"2010\",\"downloadFormat\":[{\"dataValue\":\"BRF\"},{\"dataValue\":\"DAISY\"}],\"dtbookSize\":928926,\"freelyAvailable\":0,\"images\":1,\"isbn13\":\"9781446244333\",\"language\":[{\"dataValue\":\"English US\"}],\"publishDate\":\"09122012\",\"publisher\":\"SAGE Press\",\"quality\":\"Publisher Quality\",\"title\":\"Play and Learning in the Early Years\"}","type":"ISBNLookupResponse.bookshare.book.metadata"}, {}],
 layoutBox1: ["wm.Layout", {"horizontalAlign":"left","verticalAlign":"top"}, {}, {
-dataForm1: ["wm.DataForm", {"desktopHeight":"100%","fitToContentHeight":true,"height":"373px","isCompositeKey":false,"layoutKind":"left-to-right","mobileHeight":"100%","readonly":true,"type":"PopularBookList.bookshare.book.list.result"}, {}, {
-dataForm1EditorFormPanel: ["wm.FormPanel", {"height":"100%","type":"wm.FormPanel"}, {}, {
-titleEditor1: ["wm.Text", {"caption":"Title","captionSize":"120px","changeOnKey":true,"dataValue":"","desktopHeight":"26px","emptyValue":"emptyString","formField":"title","height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}],
-authorList: ["wm.Text", {"caption":"Authors","captionSize":"120px","dataValue":undefined,"displayValue":"","emptyValue":"emptyString","height":"35px","readonly":true,"width":"100%"}, {}],
-freelyAvailableEditor1: ["wm.Number", {"caption":"FreelyAvailable","captionSize":"120px","changeOnKey":true,"dataValue":0,"desktopHeight":"26px","emptyValue":"zero","formField":"freelyAvailable","height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}],
-downloadFormatOneToMany1Panel: ["wm.Panel", {"desktopHeight":"96px","enableTouchHeight":true,"height":"100px","horizontalAlign":"left","layoutKind":"left-to-right","mobileHeight":"100px","verticalAlign":"middle","width":"100%"}, {}, {
-downloadFormatOneToMany1: ["wm.OneToMany", {"caption":undefined,"captionAlign":"left","captionPosition":undefined,"captionSize":"20px","dataValue":undefined,"desktopHeight":"26px","displayField":"dataValue","formField":"downloadFormat","minMobileHeight":100,"width":"100%"}, {}],
-button1: ["wm.Button", {"caption":"Download","height":"40px","margin":"4","width":"100px"}, {}]
+panel3: ["wm.Panel", {"_classes":{"domNode":["BookHeaderPanel"]},"fitToContentHeight":true,"height":"60px","horizontalAlign":"left","layoutKind":"left-to-right","verticalAlign":"top","width":"100%"}, {}, {
+titleLabel: ["wm.Label", {"autoSizeHeight":true,"height":"44px","padding":"4","singleLine":false,"width":"100%"}, {}, {
+binding: ["wm.Binding", {}, {}, {
+wire: ["wm.Wire", {"expression":undefined,"source":"designTimeData.title","targetProperty":"caption"}, {}]
+}]
 }],
-briefSynopsisEditor1: ["wm.Text", {"autoSizeHeight":true,"caption":"BriefSynopsis","captionSize":"120px","changeOnKey":true,"dataValue":"","desktopHeight":"26px","emptyValue":"emptyString","formField":"briefSynopsis","height":"20px","mobileHeight":"93px","readonly":true,"required":undefined,"width":"100%"}, {}],
-isbn13Editor1: ["wm.Text", {"caption":"Isbn13","captionSize":"120px","changeOnKey":true,"dataValue":"","desktopHeight":"26px","emptyValue":"emptyString","formField":"isbn13","height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}],
-publisherEditor1: ["wm.Text", {"caption":"Publisher","captionSize":"120px","changeOnKey":true,"dataValue":"","desktopHeight":"26px","emptyValue":"emptyString","formField":"publisher","height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}],
-availableToDownloadEditor1: ["wm.Number", {"caption":"AvailableToDownload","captionSize":"120px","changeOnKey":true,"dataValue":0,"desktopHeight":"26px","emptyValue":"zero","formField":"availableToDownload","height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}],
-dtbookSizeEditor1: ["wm.Number", {"caption":"DtbookSize","captionSize":"120px","changeOnKey":true,"dataValue":0,"desktopHeight":"26px","emptyValue":"zero","formField":"dtbookSize","height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}]
+bookDownloadButton: ["wm.Button", {"caption":"Download","height":"100%","margin":"8","minHeight":60,"minMobileHeight":60,"width":"100px"}, {"onclick":"bookDownloadLayer"}]
+}],
+dataForm1EditorFormPanel: ["wm.FormPanel", {"captionAlign":"left","captionSize":"100px","height":"100%"}, {}, {
+panel2: ["wm.Panel", {"autoScroll":true,"height":"100%","horizontalAlign":"left","verticalAlign":"top","width":"100%"}, {}, {
+panel1: ["wm.Panel", {"fitToContentHeight":true,"height":"34px","horizontalAlign":"left","layoutKind":"left-to-right","verticalAlign":"top","width":"100%"}, {}, {
+label1: ["wm.Label", {"caption":"Author","padding":"4","styles":{"fontWeight":"bold"},"width":"100px"}, {}],
+authorListLabel: ["wm.Label", {"autoSizeHeight":true,"display":"Array","height":"34px","padding":"4","singleLine":false,"width":"100%"}, {}, {
+format: ["wm.ArrayFormatter", {}, {}],
+binding: ["wm.Binding", {}, {}, {
+wire: ["wm.Wire", {"expression":undefined,"source":"designTimeData.author","targetProperty":"caption"}, {}]
+}]
+}]
+}],
+qualityEditor1: ["wm.Text", {"caption":"Quality","captionAlign":"left","changeOnKey":true,"displayValue":"Publisher Quality","emptyValue":"emptyString","enableTouchHeight":false,"height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}, {
+binding: ["wm.Binding", {}, {}, {
+wire: ["wm.Wire", {"expression":undefined,"source":"designTimeData.quality","targetProperty":"dataValue"}, {}]
+}]
+}],
+publisherEditor1: ["wm.Text", {"caption":"Publisher","captionAlign":"left","changeOnKey":true,"displayValue":"SAGE Press","emptyValue":"emptyString","enableTouchHeight":false,"height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}, {
+binding: ["wm.Binding", {}, {}, {
+wire: ["wm.Wire", {"expression":undefined,"source":"designTimeData.publisher","targetProperty":"dataValue"}, {}]
+}]
+}],
+publishDateEditor1: ["wm.Text", {"caption":"Published","captionAlign":"left","changeOnKey":true,"displayValue":"09/12/2012","emptyValue":"emptyString","enableTouchHeight":false,"formatter":undefined,"height":"35px","readonly":true,"required":undefined,"width":"100%"}, {}, {
+binding: ["wm.Binding", {}, {}, {
+wire: ["wm.Wire", {"expression":"${designTimeData.publishDate}.substr(0,2) + \"/\" + ${designTimeData.publishDate}.substr(2,2) + \"/\" + ${designTimeData.publishDate}.substr(4)","targetProperty":"dataValue"}, {}]
+}]
+}],
+completeSynopsisEditor1: ["wm.Text", {"autoSizeHeight":true,"caption":"Synopsis","captionAlign":"left","captionPosition":"top","captionSize":"16px","changeOnKey":true,"displayValue":"Providing high quality play experiences is an essential part of good early years education, but this can pose a challenge for practitioners who face pressure from a more didactic primary curriculum, and from parents worried that their children will fail to acquire essential skills and knowledge.  By helping the reader to develop their understanding of the complex relationships between play and learning, this book examines current theoretical perspectives on play, alongside examples of recent and innovative play research from a range of disciplinary and methodological perspectives.  With contributions from leading play scholars, it brings together theory, research, policy and practice in relation to play and learning in early years settings.  The emphasis is on the relationship between play and learning, and play and pedagogy, and the need to understand these dimensions more substantially in order to teach with confidence.  Included are chapters on: - the influence of play on thinking, problem-solving and creativity - cooperative play and learning - play, risk and outdoor learning - learning to play in cultural context There are chapter objectives, reflective points, reflective tasks and suggestions for further reading throughout, to facilitate critical thinking and encourage independent study.  Suitable for early years practitioners, early childhood students at undergraduate and postgraduate levels, and all those who work with and care for young children, this is an exciting and thought-provoking book.","emptyValue":"emptyString","enableTouchHeight":false,"height":"35px","maxHeight":100000,"readonly":true,"required":undefined,"width":"100%"}, {}, {
+binding: ["wm.Binding", {}, {}, {
+wire: ["wm.Wire", {"expression":undefined,"source":"designTimeData.completeSynopsis","targetProperty":"dataValue"}, {}]
+}]
+}]
 }]
 }]
 }]
 };
 
-BookDetails.prototype._cssText = '';
+BookDetails.prototype._cssText = '.BookHeaderPanel {\
+background-color: #febd57;\
+}\
+.BookHeaderPanel .wmlabel {\
+font-weight: bold ;\
+font-size: 1.4Em ;\
+color: white ;\
+}\
+';
 BookDetails.prototype._htmlText = '';
