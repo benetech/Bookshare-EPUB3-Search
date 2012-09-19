bd6b3923d18eaaae68a244952f0457452ef6fe57
Derek Henninger <dhenninger@vmware.com>
2012-09-19 13:40:28 -0700

    Improved About page
    Added info that app is Powered by WaveMaker.  Also improved overall
    layout.

--- projects/BookShare/webapproot/pages/About/About.widgets.js
+++ projects/BookShare/webapproot/pages/About/About.widgets.js
@@ -1,7 +1,13 @@
 About.widgets = {
-	layoutBox1: ["wm.Layout", {"horizontalAlign":"left","verticalAlign":"top"}, {}, {
-		html1: ["wm.Html", {"autoSizeHeight":true,"height":"112px","html":"<font face=\"verdanda\" size=\"3\"><p>This BookShare application was developed by VMware employees pro bono as part of the Service Learning initiative sponsored by the <a href=\"http://www.vmware.com/company/foundation.html\">VMware Foundation</a>.</p>\n</font>","margin":"0,5,0,5","minDesktopHeight":15}, {}],
-		picture1: ["wm.Picture", {"aspect":"h","height":"115px","source":"resources/images/logos/VMW_10Q3_ICON_Foundation_R4.png","width":"319px"}, {}],
-		html2: ["wm.Html", {"autoSizeHeight":true,"height":"167px","html":"<font face=\"verdanda\" size=\"2\">\n<p>The <a href=\"https://www.bookshare.org/\">Bookshare</a> project is supported by the U.S. Department of Education, Office of Special Education Programs (Cooperative Agreement #H327K070001). Opinions expressed herein are those of the authors and do not necessarily represent the position of the U.S. Department of Education.</p>\n<p>Bookshare © 2012 <a href=\"http://www.benetech.org/\">Benetech</a>. All rights reserved. Bookshare® is a registered trademark of Benetech.</p></font>","margin":"0,5,0,5","minDesktopHeight":15}, {}]
+	layoutBox1: ["wm.Layout", {"_classes":{"domNode":["wm_Attribution_center"]},"horizontalAlign":"left","verticalAlign":"top"}, {}, {
+		panel1: ["wm.Panel", {"height":"152px","horizontalAlign":"left","layoutKind":"left-to-right","verticalAlign":"middle","width":"100%"}, {}, {
+			html1: ["wm.Html", {"height":"152px","html":"<font face=\"verdanda\" size=\"3\">\n<p>This Bookshare application was developed by VMware employees pro bono as part of the Service Learning initiative sponsored by the <a href=\"http://www.vmware.com/company/foundation.html\">VMware Foundation</a>.</p>\n</font>","minDesktopHeight":15,"padding":"0,5,0,5"}, {}],
+			picture1: ["wm.Picture", {"aspect":"h","height":"115px","imageList":"app.silkIconList","link":"http://www.vmware.com/company/foundation.html","source":"resources/images/logos/VMW_10Q3_ICON_Foundation_R4.png","width":"115px"}, {}]
+		}],
+		panel2: ["wm.Panel", {"height":"94px","horizontalAlign":"left","layoutKind":"left-to-right","padding":"0,5,0,5","verticalAlign":"top","width":"100%"}, {}, {
+			picture2: ["wm.Picture", {"aspect":"h","height":"85px","link":"http://www.wavemaker.com/","source":"resources/images/logos/Other/wavemaker_64x64.png","width":"85px"}, {}],
+			html3: ["wm.Html", {"autoSizeHeight":true,"height":"94px","html":"<font face=\"arial\" size=\"4\">\nPowered by WaveMaker\n</font>\n<font face=\"verdanda\" size=\"3\">\n<p>This Bookshare application was built using <a href=\"www.wavemaker.com\">WaveMaker</a>.</p>\n</font>","minDesktopHeight":15,"padding":"0,0,0,2"}, {}]
+		}],
+		html2: ["wm.Html", {"autoSizeHeight":true,"height":"167px","html":"<font face=\"verdanda\" size=\"2\">\n<p>The <a href=\"https://www.bookshare.org/\">Bookshare</a> project is supported by the U.S. Department of Education, Office of Special Education Programs (Cooperative Agreement #H327K070001). Opinions expressed herein are those of the authors and do not necessarily represent the position of the U.S. Department of Education.</p>\n<p>Bookshare © 2012 <a href=\"http://www.benetech.org/\">Benetech</a>. All rights reserved. Bookshare® is a registered trademark of Benetech.</p></font>","minDesktopHeight":15,"padding":"0,5,0,5"}, {}]
 	}]
 }
