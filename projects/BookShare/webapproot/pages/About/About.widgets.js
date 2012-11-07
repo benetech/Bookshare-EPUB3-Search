c0d1eb818264200067e05ce2938e725b27e82989
Derek Henninger <dhenninger@vmware.com>
2012-11-06 17:16:38 -0800

    Changed layout of About page to display better on Tablet and Desktop devices.  Previous layout was designed for phone only.

--- projects/BookShare/webapproot/pages/About/About.widgets.js
+++ projects/BookShare/webapproot/pages/About/About.widgets.js
@@ -1,12 +1,12 @@
 About.widgets = {
 	layoutBox1: ["wm.Layout", {"_classes":{"domNode":["wm_Attribution_center"]},"horizontalAlign":"left","verticalAlign":"top"}, {}, {
-		panel1: ["wm.Panel", {"height":"152px","horizontalAlign":"left","layoutKind":"left-to-right","verticalAlign":"middle","width":"100%"}, {}, {
-			html1: ["wm.Html", {"height":"152px","html":"<font face=\"verdanda\" size=\"3\">\n<p>This Bookshare application was developed by VMware employees pro bono as part of the Service Learning initiative sponsored by the <a href=\"http://www.vmware.com/company/foundation.html\">VMware Foundation</a>.</p>\n</font>","minDesktopHeight":15,"padding":"0,5,0,5"}, {}],
-			picture1: ["wm.Picture", {"aspect":"h","height":"115px","imageList":"app.silkIconList","link":"http://www.vmware.com/company/foundation.html","source":"resources/images/logos/VMW_10Q3_ICON_Foundation_R4.png","width":"115px"}, {}]
+		panel1: ["wm.Panel", {"height":"60%","horizontalAlign":"left","layoutKind":"left-to-right","verticalAlign":"middle","width":"100%"}, {}, {
+			html1: ["wm.Html", {"height":"152px","html":"<font face=\"verdanda\" size=\"3\">\n<p>This BookShare application was developed by VMware employees pro bono as part of the Service Learning initiative sponsored by the <a href=\"http://www.vmware.com/company/foundation.html\">VMware Foundation</a>.</p>\n</font>","minDesktopHeight":15,"padding":"0,5,0,5","width":"65%"}, {}],
+			picture1: ["wm.Picture", {"aspect":"h","height":"60%","imageList":"app.silkIconList","link":"http://www.vmware.com/company/foundation.html","source":"resources/images/logos/VMW_10Q3_ICON_Foundation_R4.png","width":"35%"}, {}]
 		}],
-		panel2: ["wm.Panel", {"height":"94px","horizontalAlign":"left","layoutKind":"left-to-right","padding":"0,5,0,5","verticalAlign":"top","width":"100%"}, {}, {
-			picture2: ["wm.Picture", {"aspect":"h","height":"85px","link":"http://www.wavemaker.com/","source":"resources/images/logos/Other/wavemaker_64x64.png","width":"85px"}, {}],
-			html3: ["wm.Html", {"autoSizeHeight":true,"height":"94px","html":"<font face=\"arial\" size=\"4\">\nPowered by WaveMaker\n</font>\n<font face=\"verdanda\" size=\"3\">\n<p>This Bookshare application was built using <a href=\"www.wavemaker.com\">WaveMaker</a>.</p>\n</font>","minDesktopHeight":15,"padding":"0,0,0,2"}, {}]
+		panel2: ["wm.Panel", {"height":"40%","horizontalAlign":"left","layoutKind":"left-to-right","padding":"0,5,0,5","verticalAlign":"middle","width":"100%"}, {}, {
+			picture2: ["wm.Picture", {"aspect":"h","height":"70%","link":"http://www.wavemaker.com/","source":"resources/images/logos/Other/wm_bug_150.png","width":"35%"}, {}],
+			html3: ["wm.Html", {"autoSizeHeight":true,"height":"94px","html":"<font face=\"arial\" size=\"4\">\nPowered by WaveMaker\n</font>\n<font face=\"verdanda\" size=\"3\">\n<p>This BookShare application was built using <a href=\"www.wavemaker.com\">WaveMaker</a>.</p>\n</font>","minDesktopHeight":15,"padding":"0,0,0,2","width":"65%"}, {}]
 		}],
 		html2: ["wm.Html", {"autoSizeHeight":true,"height":"167px","html":"<font face=\"verdanda\" size=\"2\">\n<p>The <a href=\"https://www.bookshare.org/\">Bookshare</a> project is supported by the U.S. Department of Education, Office of Special Education Programs (Cooperative Agreement #H327K070001). Opinions expressed herein are those of the authors and do not necessarily represent the position of the U.S. Department of Education.</p>\n<p>Bookshare © 2012 <a href=\"http://www.benetech.org/\">Benetech</a>. All rights reserved. Bookshare® is a registered trademark of Benetech.</p></font>","minDesktopHeight":15,"padding":"0,5,0,5"}, {}]
 	}]
